d23a12095cdf38b37c395b0e58a51e34a7512509
Kevin Hu <hellokevinhu@icloud.com>
2024-08-19 22:15:54 +0800

    Update e2e command

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -1,14 +1,67 @@
 describe('Reservations', () => {
+  let jwt: string;
+
   beforeAll(async () => {
     const user = {
       email: 'sleeprnestapp@gmail.com',
       password: 'StrongPassword123!@',
     };
-    await fetch('http://auth:3001', {
+    await fetch('http://auth:3001/users', {
       method: 'POST',
       body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
+    const response = await fetch('http://auth:3001/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    jwt = await response.text();
   });
 
-  test('Create', () => {});
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          Authentication: jwt,
+        },
+      },
+    );
+    const reservation = await responseGet.json();
+    expect(createdReservation).toEqual(reservation);
+  });
+
+  const createReservation = async () => {
+    const responseCreate = await fetch(
+      'http://reservations:3000/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+        body: JSON.stringify({
+          startDate: '2023-12-20T00:00:00-08:00',
+          endDate: '2023-12-25T00:00:00-08:00',
+          charge: {
+            amount: 33.0,
+            card: {
+              cvc: '413',
+              exp_month: 12,
+              exp_year: 2027,
+              number: '4242 4242 4242 4242',
+            },
+          },
+        }),
+      },
+    );
+    expect(responseCreate.ok).toBeTruthy();
+    return responseCreate.json();
+  };
 });
