649594b8bfc39dd5aef37c08efb054a63c4e74af
Kevin Hu <hellokevinhu@icloud.com>
2024-09-12 20:47:23 +0800

    GraphQL gateway

--- libs/common/src/dto/create-charge.dto.ts
+++ libs/common/src/dto/create-charge.dto.ts
@@ -6,14 +6,18 @@ import {
 } from 'class-validator';
 import { CardDto } from './card.dto';
 import { Type } from 'class-transformer';
+import { Field, InputType } from '@nestjs/graphql';
 
+@InputType()
 export class CreateChargeDto {
   @IsDefined()
   @IsNotEmptyObject()
   @ValidateNested()
   @Type(() => CardDto)
+  @Field(() => CardDto)
   card: CardDto; // For stripe test mode, this property is unnecessary. Here it persists as a example.
 
   @IsNumber()
+  @Field()
   amount: number;
 }
